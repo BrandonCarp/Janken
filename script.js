e5e37361eb1ee772c114904cc9bbc943a911c7e2
Brandon <69767056+OtakuxCode@users.noreply.github.com>
2021-06-17 15:59:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
 // Buttons
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissor = document.querySelector('.scissor');
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let scissor = document.querySelector('.scissor');
+
 
 // Victory Output
 let player = document.querySelector('.player');
 let computer = document.querySelector('.computer');
-let outCome = document.querySelector('outCome');
+let outCome = document.querySelector('.outCome');
 
 
-// What is needed
-// On click set math.random() maybe floor
-// To each button , * 3 I think?
-// Set up outcomes for each
-// Possibly able to use switch statements
-// Out comes set in vars
-// Vars used to output user/pc/victor
 
-rock.addEventListener('click', rockClick);
-// paper.addEventListener('click', paperClick);
-// scissor.addEventListener('click', scissorClick);
+const choice = ['rock', 'paper', 'scissor'];
+// let random = choice[Math.floor(Math.random() * choice.length)];
+// let computerChoice = random;    
+let random;
+let computerChoice;
 
-function rockClick(e) {
-  player = Math.random() * 3;
-  console.log(player);
-}
+rock.addEventListener('click', function() {
+ random = choice[Math.floor(Math.random() * choice.length)];
+ computerChoice = random;  
+  if(computerChoice === 'rock') {
+    player.innerText = 'You Choose Rock';
+    computer.innerText = 'The Computer Choose Rock';
+    outCome.innerText = 'Its a Draw!';
+  } else if(computerChoice === 'paper') {
+    player.innerText = 'You Choose Rock';
+    computer.innerText = 'The Computer Choose Paper';
+    outCome.innerText = 'The Computer Wins!';
+  } else {
+    player.innerText = 'You Choose Rock';
+    computer.innerText = 'The Computer Choose Scissors';
+    outCome.innerText = 'You Win!';
+  }
+});
+
+paper.addEventListener('click', function() {
+  random = choice[Math.floor(Math.random() * choice.length)];
+  computerChoice = random; 
+ if(computerChoice === 'paper') {
+   player.innerText = 'You Choose Paper';
+   computer.innerText = 'The Computer Choose Paper';
+   outCome.innerText = 'Its a Draw!';
+ } else if(computerChoice === 'scissor') {
+   player.innerText = 'You Choose Paper';
+   computer.innerText = 'The Computer Choose Scissors';
+   outCome.innerText = 'The Computer Wins!';
+ } else {
+   player.innerText = 'You Choose Paper';
+   computer.innerText = 'The Computer Choose rock';
+   outCome.innerText = 'You Win!';
+ }
+});
+
+
+scissor.addEventListener('click', function() {
+  random = choice[Math.floor(Math.random() * choice.length)];
+  computerChoice = random; 
+ if(computerChoice === 'scissor') {
+   player.innerText = `You Choose Scissors`;
+   computer.innerText = 'The Computer Choose Scissors';
+   outCome.innerText = 'Its a Draw!';
+ } else if(computerChoice === 'rock') {
+   player.innerText = 'You Choose Scissors';
+   computer.innerText = 'The Computer Choose Rock';
+   outCome.innerText = 'The Computer Wins!';
+ } else {
+   player.innerText = 'You Choose Scissors';
+   computer.innerText = 'The Computer Choose Paper';
+   outCome.innerText = 'You Win!';
+ }
+});
+
+
+
+
+
+
 
 
 
